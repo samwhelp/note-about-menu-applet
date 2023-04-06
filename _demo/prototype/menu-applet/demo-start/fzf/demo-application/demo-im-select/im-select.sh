f258f4512c4280f1173601123978cc308b0337fa
samwhelp <samwhelp@users.noreply.github.com>
2023-04-06 20:21:05 +0800

    fix

---
+++ _demo/prototype/menu-applet/demo-start/fzf/demo-application/demo-im-select/im-select.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+
+##
+## https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+##
+
+THE_DEFAULT_DISTRO="${THE_DEFAULT_DISTRO:=ubuntu}"
+
+main_distro () {
+	echo "${THE_DEFAULT_DISTRO}"
+}
+
+menu_list () {
+
+cat << EOF
+fcitx
+fcitx5
+ibus
+gcin
+hime
+
+
+EOF
+
+}
+
+menu_factory () {
+	fzf
+}
+
+menu_start () {
+	 menu_list | menu_factory
+}
+
+demo_applet () {
+
+	local selected=$(menu_start)
+	
+	if [ -z "${selected}" ]; then
+		echo "## Not selected ##"
+		
+		return 0
+	fi
+
+	echo "## Selected: ${selected}"
+
+	
+	local input_method_framework="${selected}"
+
+	echo "## Use IM: ${input_method_framework}"
+
+	im_ctrl_set "${input_method_framework}"
+
+}
+
+
+im_ctrl_set () {
+
+	local agent
+
+	#agent="ubuntu"
+	#agent="arch"
+
+	agent="$(main_distro)"
+
+	local delegate="im_ctrl_set_for_${agent}"
+
+	"${delegate}" "${1}"
+
+}
+
+im_ctrl_set_for_ubuntu () {
+
+	echo
+	echo "## Config: im_ctrl_set_for_ubuntu"
+	echo
+
+	##
+	## https://wiki.archlinux.org/title/feh
+	##
+
+	local input_method_framework="${1}"
+
+	echo
+	echo "im-config -n \"${input_method_framework}\""
+	im-config -n "${input_method_framework}"
+
+
+	echo
+	echo "cat \"${HOME}/.xinputrc\""
+	echo
+	cat "${HOME}/.xinputrc"
+
+	echo
+
+	return 0
+}
+
+
+
+im_ctrl_set_for_arch () {
+
+	echo
+	echo "## TODO: im_ctrl_set_for_arch"
+	echo
+
+	local input_method_framework="${1}"
+
+	echo
+	echo "## input_method_framework: ${input_method_framework}"
+
+
+	echo
+
+	return 0
+}
+
+
+__main__ () {
+	demo_applet
+}
+
+__main__
---
+++ _demo/prototype/menu-applet/demo-start/jgmenu/demo-application/demo-im-select/README.md
@@ -0,0 +1,28 @@
+
+
+# demo-im-select
+
+
+## Usage
+
+
+### ubuntu
+
+run
+
+``` sh
+./im-select.sh
+```
+
+or run
+
+``` sh
+THE_DEFAULT_DISTRO=ubuntu ./im-select.sh
+```
+
+
+### arch
+
+``` sh
+THE_DEFAULT_DISTRO=arch ./im-select.sh
+```
