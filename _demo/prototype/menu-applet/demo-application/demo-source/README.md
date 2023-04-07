cc3a6485ca8aa87ab9b8bb14529b4fd0d0086bd2
samwhelp <samwhelp@users.noreply.github.com>
2023-04-07 10:00:34 +0800

    fix

---
+++ _demo/prototype/menu-applet/demo-application/demo-source/README.md
@@ -0,0 +1,48 @@
+
+
+# demo-demo
+
+
+## Usage
+
+
+## Usage / Menu Factory
+
+
+### fzf
+
+run
+
+``` sh
+./demo.sh
+```
+
+or run
+
+``` sh
+THE_DEFAULT_MENU_FACTORY=fzf ./demo.sh
+```
+
+### dmenu
+
+run
+
+``` sh
+THE_DEFAULT_MENU_FACTORY=dmenu ./demo.sh
+```
+
+### rofi
+
+run
+
+``` sh
+THE_DEFAULT_MENU_FACTORY=rofi ./demo.sh
+```
+
+### jgmenu
+
+run
+
+``` sh
+THE_DEFAULT_MENU_FACTORY=jgmenu ./demo.sh
+```
---
+++ _demo/prototype/menu-applet/demo-application/demo-source/demo.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+
+##
+## https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+##
+
+THE_DEFAULT_MENU_FACTORY="${THE_DEFAULT_MENU_FACTORY:=fzf}"
+
+main_menu_factory () {
+	echo "${THE_DEFAULT_MENU_FACTORY}"
+}
+
+
+
+
+menu_list () {
+	ls -1 ./profiles
+}
+
+menu_factory () {
+	local agent
+
+	#agent="fzf"
+	#agent="dmenu"
+	#agent="rofi"
+	#agent="jgmenu"
+
+	agent=$(main_menu_factory)
+
+	local delegate="menu_factory_by_${agent}"
+
+	"${delegate}"
+}
+
+menu_factory_by_fzf () {
+	fzf
+}
+
+menu_factory_by_dmenu () {
+	dmenu -b -l 10
+}
+
+menu_factory_by_rofi () {
+	rofi -dmenu
+}
+
+menu_factory_by_jgmenu () {
+	jgmenu --simple --center --no-spawn
+}
+
+menu_start () {
+	 menu_list | menu_factory
+}
+
+menu_start () {
+	 menu_list | menu_factory
+}
+
+demo_applet () {
+
+	local selected=$(menu_start)
+	
+	if [ -z "${selected}" ]; then
+		echo "## Not selected ##"
+		
+		return 0
+	fi
+
+	echo "## Selected: ${selected}"
+
+	
+	local source_file_path="./profiles/${selected}/config.sh"
+	
+	if ! [ -f "${source_file_path}" ]; then
+		echo "## Source File Not Exist: ${source_file_path}"
+		
+		return 0
+	fi	
+	
+	source "${source_file_path}"
+	
+	echo "Theme: ${THE_THEME_NAME}"
+
+}
+
+
+
+
+__main__ () {
+	demo_applet
+}
+
+__main__
