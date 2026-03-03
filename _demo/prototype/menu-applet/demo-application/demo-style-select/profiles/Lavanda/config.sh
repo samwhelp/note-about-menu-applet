68f07ceb553e39f620f2db5cfb8be4b0d9696925
samwhelp <samwhelp@users.noreply.github.com>
2026-03-04 06:42:35 +0800

    fix

---
+++ _demo/prototype/menu-applet/demo-application/demo-style-select/profiles/Lavanda/config.sh
@@ -0,0 +1,6 @@
+THE_GHOME_SHELL_THEME_NAME="Lavanda-Sea-Dark"
+THE_WM_THEME_NAME="Lavanda-Sea-Dark"
+THE_GTK_THEME_NAME="Lavanda-Sea-Dark"
+THE_ICON_THEME_NAME="Citrus-red-dark"
+THE_CURSOR_THEME_NAME="Vimix-cursors"
+THE_CURSOR_SIZE="24"
