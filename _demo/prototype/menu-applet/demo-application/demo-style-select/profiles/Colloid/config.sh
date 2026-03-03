b29d32545b9e058d6d3c2b8e8f560cc889eb1cbf
samwhelp <samwhelp@users.noreply.github.com>
2026-03-04 06:38:56 +0800

    fix

---
+++ _demo/prototype/menu-applet/demo-application/demo-style-select/profiles/Colloid/config.sh
@@ -0,0 +1,6 @@
+THE_GHOME_SHELL_THEME_NAME="Colloid-Dark"
+THE_WM_THEME_NAME="Colloid-Dark"
+THE_GTK_THEME_NAME="Colloid-Dark"
+THE_ICON_THEME_NAME="Colloid-Dark"
+THE_CURSOR_THEME_NAME="Colloid-dark-cursors"
+THE_CURSOR_SIZE="24"
