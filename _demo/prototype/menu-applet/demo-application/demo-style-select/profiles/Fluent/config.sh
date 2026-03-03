97e14df4400dbd30b068e5e08c93a0f4a1fc02ac
samwhelp <samwhelp@users.noreply.github.com>
2026-03-04 06:36:47 +0800

    fix

---
+++ _demo/prototype/menu-applet/demo-application/demo-style-select/profiles/Fluent/config.sh
@@ -0,0 +1,6 @@
+THE_GHOME_SHELL_THEME_NAME="Fluent-round-Dark"
+THE_WM_THEME_NAME="Fluent-round-Dark"
+THE_GTK_THEME_NAME="Fluent-round-Dark"
+THE_ICON_THEME_NAME="Fluent-dark"
+THE_CURSOR_THEME_NAME="Fluent-dark-cursors"
+THE_CURSOR_SIZE="24"
